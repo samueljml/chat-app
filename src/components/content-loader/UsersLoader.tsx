d492074d0a4f9df246859ab9ccef75350257080f
Samuel Andrade <samuelfigueiredodeandrade@gmail.com>
2021-07-27 17:42:18 -0300

    [INT-40] - Message and User Loader

---
+++ src/components/content-loader/UsersLoader.tsx
@@ -0,0 +1,21 @@
+import ContentLoader from "react-content-loader";
+
+export const UsersLoader = () => (
+	<ContentLoader
+		speed={2}
+		width={367}
+		height={164}
+		viewBox="0 0 367 164"
+		backgroundColor="#f3f3f3"
+		foregroundColor="#c2c2c2"
+	>
+		<circle cx="48" cy="40" r="25" />
+		<rect x="84" y="21" rx="0" ry="0" width="156" height="16" />
+		<rect x="84" y="46" rx="0" ry="0" width="140" height="16" />
+		<circle cx="320" cy="40" r="19" />
+		<circle cx="48" cy="120" r="25" />
+		<rect x="84" y="100" rx="0" ry="0" width="156" height="16" />
+		<rect x="84" y="125" rx="0" ry="0" width="140" height="16" />
+		<circle cx="320" cy="120" r="19" />
+	</ContentLoader>
+);
