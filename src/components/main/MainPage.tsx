f7f18fc7c238f1a65d056085c76349c7a1c39bf2
Samuel Andrade <samuelfigueiredodeandrade@gmail.com>
2021-08-07 18:32:40 -0300

    Adding GET User

--- src/components/main/MainPage.tsx
+++ src/components/main/MainPage.tsx
@@ -1,5 +1,8 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../api";
 import "../../App.css";
+import { executePromise } from "../../common/Utils";
 import { ChatTitle } from "../chat/ChatTitle";
 import { ChatForm } from "../chat/form/ChatForm";
 import { MainPageContext } from "../context/MainPageContext";
@@ -18,9 +21,34 @@ export interface User {
 	imageUrl: string;
 }
 
+interface UserProps {
+	data: User;
+}
+
+interface UserId {
+	loggedUserId: string;
+}
+
 export const MainPage = () => {
 	const [inputSearchValue, setInputSearchValue] = useState("");
-	const { isAddUserActive } = useContext(MainPageContext);
+	const { isAddUserActive, setUser } = useContext(MainPageContext);
+	let { loggedUserId } = useParams<UserId>();
+
+	const showUser = async () => {
+		const uri = `/user/${loggedUserId}`;
+
+		const [response] = await executePromise<UserProps>(() => api.get(uri));
+
+		if (response) {
+			setUser(response.data);
+		}
+	};
+
+	const handle = () => {
+		showUser();
+	};
+
+	useEffect(handle, []);
 
 	return (
 		<div className="chat-container">
